a923a4e18cafa5d0fd053f6e6d0dc9c73a83045f
Jean Silva <jeancarlos.silva@me.com>
2020-04-10 17:58:44 -0300

    Added developer webserver

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+
+app.listen(port, err => err ? console.log(err) : open(`http://localhost:${port}`));
